febaaa42226210f3476be3ab2b9504b3bccfde6c
Harry Galliano <harrygalliano97@gmail.com>
2019-05-15 12:36:09 +0100

    Testing for successful login

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -1,56 +1,66 @@
 feature 'Sign_up page' do
-    scenario 'indicates the password do not match' do
-      visit '/users/new'
-      fill_in 'user_name', with: 'Test Name'
-      fill_in 'user_email', with: 'test@test.com'
-      fill_in 'user_password', with: '123456'
-      fill_in 'user_password_confirmation', with: '1234567'
-      click_button 'Create'
-      expect(page).to have_content 'Password confirmation doesn\'t match Password'
-    end
-  
-
-    scenario 'checks that the user enters a name' do
-      visit '/users/new'
-      fill_in 'user_name', with: ''
-      fill_in 'user_email', with: 'test@test.com'
-      fill_in 'user_password', with: '123456'
-      fill_in 'user_password_confirmation', with: '123456'
-      click_button 'Create'
-      expect(page).to have_content 'Name can\'t be blank'
-    end
+  scenario 'indicates the password do not match' do
+    visit '/users/new'
+    fill_in 'user_name', with: 'Test Name'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: '123456'
+    fill_in 'user_password_confirmation', with: '1234567'
+    click_button 'Create'
+    expect(page).to have_content 'Password confirmation doesn\'t match Password'
+  end
 
 
-
-    scenario 'checks that the user enters an email' do
-      visit '/users/new'
-      fill_in 'user_name', with: 'Harry'
-      fill_in 'user_email', with: ''
-      fill_in 'user_password', with: '123456'
-      fill_in 'user_password_confirmation', with: '123456'
-      click_button 'Create'
-      expect(page).to have_content 'Email can\'t be blank'
-    end
-
-
-    scenario 'checks that the user enters a password' do
-      visit '/users/new'
-      fill_in 'user_name', with: 'Harry'
-      fill_in 'user_email', with: 'test@test.com'
-      fill_in 'user_password', with: ''
-      fill_in 'user_password_confirmation', with: ''
-      click_button 'Create'
-      expect(page).to have_content 'Password can\'t be blank'
-    end
+  scenario 'checks that the user enters a name' do
+    visit '/users/new'
+    fill_in 'user_name', with: ''
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: '123456'
+    fill_in 'user_password_confirmation', with: '123456'
+    click_button 'Create'
+    expect(page).to have_content 'Name can\'t be blank'
+  end
 
 
-    scenario 'checks that the user enters a password that is long enough' do
-      visit '/users/new'
-      fill_in 'user_name', with: ''
-      fill_in 'user_email', with: 'test@test.com'
-      fill_in 'user_password', with: '12345'
-      fill_in 'user_password_confirmation', with: '12345'
-      click_button 'Create'
-      expect(page).to have_content 'Password is too short (minimum is 6 characters)'
-    end
+
+  scenario 'checks that the user enters an email' do
+    visit '/users/new'
+    fill_in 'user_name', with: 'Harry'
+    fill_in 'user_email', with: ''
+    fill_in 'user_password', with: '123456'
+    fill_in 'user_password_confirmation', with: '123456'
+    click_button 'Create'
+    expect(page).to have_content 'Email can\'t be blank'
+  end
+
+
+  scenario 'checks that the user enters a password' do
+    visit '/users/new'
+    fill_in 'user_name', with: 'Harry'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: ''
+    fill_in 'user_password_confirmation', with: ''
+    click_button 'Create'
+    expect(page).to have_content 'Password can\'t be blank'
+  end
+
+
+  scenario 'checks that the user enters a password that is long enough' do
+    visit '/users/new'
+    fill_in 'user_name', with: ''
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: '12345'
+    fill_in 'user_password_confirmation', with: '12345'
+    click_button 'Create'
+    expect(page).to have_content 'Password is too short (minimum is 6 characters)'
+  end
+
+  scenario 'checks that the user gets redirected after successful log in' do
+    visit '/users/new'
+    fill_in 'user_name', with: 'Test'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: '123456'
+    fill_in 'user_password_confirmation', with: '123456'
+    click_button 'Create'
+    expect(page).to have_content 'Test'
+  end
 end
