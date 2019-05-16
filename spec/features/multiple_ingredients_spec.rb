44c559de06de8362a0b38a27f44505ca5a47db8c
Harry Galliano <harrygalliano97@gmail.com>
2019-05-16 16:10:51 +0100

    made a feature test

--- spec/features/multiple_ingredients_spec.rb
+++ spec/features/multiple_ingredients_spec.rb
@@ -17,4 +17,27 @@ feature 'Create recipe card with multiple ingredients' do
     click_button 'Submit'
     expect(page).to have_content 'Add Method'
   end
+
+  scenario 'I can remove ingredients to a recipe I have started to make', js: true do
+    visit '/recipes/new'
+    fill_in 'recipe_name', with: 'Test recipe name'
+    fill_in 'recipe_description', with: 'Test recipe description'
+    click_button 'Create'
+
+    click_button 'More Ingredients'
+    click_button 'More Ingredients'
+
+    fill_in 'ingredients_1[name]', with: 'test name'
+    fill_in 'ingredients_1[value]', with: 1
+    fill_in 'ingredients_1[unit]', with: 'g'
+    fill_in 'ingredients_2[name]', with: 'test name 2'
+    fill_in 'ingredients_2[value]', with: 2
+    fill_in 'ingredients_2[unit]', with: 'g'
+    fill_in 'ingredients_3[name]', with: 'test name 3'
+    fill_in 'ingredients_3[value]', with: 3
+    fill_in 'ingredients_3[unit]', with: 'g'
+    click_button 'Less Ingredients '
+    click_button 'Submit'
+    expect(page).to have_content 'Add Method'
+  end
 end
