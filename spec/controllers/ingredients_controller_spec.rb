bd3b447dacf2446d717982e8f8be0480050513c5
Lauren <laurenqurashi@me.com>
2019-05-16 16:46:50 +0100

    Users cannot access the new recipies page if they're not logged in. Tests passing.

--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe IngredientsController, type: :controller do
   before(:each) do
     @user = User.create(name: 'test', email: 'test@test.com', password: '123456', password_confirmation: '123456')
     @recipe = Recipe.create(name: 'Lasagne', description: 'Tastes real nice', user_id: @user.id)
+    session[:user_id] = @user.id
   end
 
   describe "GET #new" do
@@ -15,11 +16,13 @@ RSpec.describe IngredientsController, type: :controller do
   end
 
   describe "POST #create" do
+
     it "adds an ingredient to ingredients table" do
       post :create, params: { id: @recipe.id,
         ingredients_1: { name: "Test ingredient", value: 3.0, unit: "test" } }
       expect(Ingredient.find_by(name: 'Test ingredient')).to_not eq nil
     end
+
     it "adds multiple ingredients to ingredients table" do
       post :create, params: { id: @recipe.id,
         ingredients_1: { name: "Test ingredient 1", value: 3.0, unit: "test" },
@@ -27,6 +30,7 @@ RSpec.describe IngredientsController, type: :controller do
       expect(Ingredient.find_by(name: 'Test ingredient 1')).to_not eq nil
       expect(Ingredient.find_by(name: 'Test ingredient 2')).to_not eq nil
     end
+
     it "redirects to method page" do
       expect(
         post :create, params: {
@@ -35,6 +39,13 @@ RSpec.describe IngredientsController, type: :controller do
             }
           }
       ).to redirect_to("/steps/#{@recipe.id}/new")
+    end
+
+    it "does not create an ingredient if user is not logged in" do
+      session.delete(:user_id)
+      expect(Ingredient).to_not receive(:create)
+      post :create, params: { id: @recipe.id,
+        ingredients_1: { name: "Test ingredient", value: 3.0, unit: "test" } }
     end
   end
 end
--- spec/controllers/steps_controller_spec.rb
+++ spec/controllers/steps_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe StepsController, type: :controller do
   before(:each) do
     @user = User.create(name: 'test', email: 'test@test.com', password: '123456', password_confirmation: '123456')
     @recipe = Recipe.create(name: 'Lasagne', description: 'Tastes real nice', user_id: @user.id)
+    session[:user_id] = @user.id
     @id = @recipe.id
     @ingredient = Ingredient.create(name: 'test ing', value: 2.0, unit: 'g', recipe_id: @id)
   end
@@ -21,16 +22,24 @@ RSpec.describe StepsController, type: :controller do
       post :create, params: { id: @id, steps_1: { description: "Test step" } }
       expect(Step.find_by(description: 'Test step')).to_not eq nil
     end
+
     it "can add multiple steps to steps table" do
       post :create, params: { id: @id, steps_1: { description: "Test step 1" },
         steps_2: { description: "Test step 2" } }
       expect(Step.find_by(description: 'Test step 1')).to_not eq nil
       expect(Step.find_by(description: 'Test step 2')).to_not eq nil
     end
+
     it "redirects to method page" do
       post :create, params: { id: @id, steps_1: { description: "Test step" } }
       expect(Step.find_by(description: 'Test step'))
         .to redirect_to("/recipes/#{@recipe.id}")
+    end
+
+    it "doesn't add step if not logged in" do
+      session.delete(:user_id)
+      expect(Step).to_not receive(:create)
+      post :create, params: { id: @id, steps_1: { description: "Test step" } }
     end
   end
 end
