34578938be773c3e3f889f0881c7c9f7f01db674
Harry Galliano <harrygalliano97@gmail.com>
2019-05-16 14:56:34 +0100

    you ar enow able to remove steps

--- app/assets/javascripts/step_form_inputs.js
+++ app/assets/javascripts/step_form_inputs.js
@@ -13,4 +13,10 @@ $(".steps.new").ready(function() {
     counter += 1
     document.getElementById(`step_${counter}`).innerHTML = step_input_builder(counter)
   });
+
+  document.getElementById('remove_step_button').addEventListener("click", function(event){
+    event.preventDefault()
+    document.getElementById(`step_${counter}`).innerHTML = ''
+    counter -= 1
+  });
 });
