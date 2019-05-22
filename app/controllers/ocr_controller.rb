27b9c3bfa792651c1c8cfdcaac7e0caac080a510
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-05-22 17:41:33 +0100

    ADD: User can upload image and get text displayed to help with entering a recipe

--- app/controllers/ocr_controller.rb
+++ app/controllers/ocr_controller.rb
@@ -3,8 +3,7 @@ class OcrController < ApplicationController
   def create
     image = params[:image].tempfile
     response = RestClient.post 'https://api.ocr.space/parse/image',
-      file: image, apikey: ENV['OCR_SPACE_API_KEY'], isCreateSearchablePdf: true,
-      isSearchablePdfHideTextLayer: true, scale: true
+      file: image, apikey: ENV['OCR_SPACE_API_KEY']
     flash[:ocr_text] = JSON.parse(response.body)['ParsedResults'][0]['ParsedText']
     redirect_to '/recipes/new'
   end
--- spec/controllers/ocr_controller_spec.rb
+++ spec/controllers/ocr_controller_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 RSpec.describe OcrController, type: :controller do
 
-  xdescribe "POST #create" do
-    it "returns http success" do
-      post :create
-      expect(response).to have_http_status(:success)
-    end
+  describe "POST #create" do
   end
 end
