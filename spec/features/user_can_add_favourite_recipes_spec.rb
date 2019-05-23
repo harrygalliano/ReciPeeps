b56fad62b265932ed131144373c92f9298587ab2
fo0s <fo0sies@gmail.com>
2019-05-23 23:50:28 +0100

    Fixed: favourites/index was being rendered properly

--- spec/features/user_can_add_favourite_recipes_spec.rb
+++ spec/features/user_can_add_favourite_recipes_spec.rb
@@ -13,13 +13,13 @@ feature "User can add recipes to favourites" do
   end
 
   scenario "logged in user can remove recipe from favourites", js: true do
-  sign_up
-  log_in
-  create_recipe
-  click_button "Favourite"
-  visit "/mycookbook"
-  expect(page).to have_content "Test recipe name"
-  click_button "Unfavourite"
-  expect(page).to_not have_content "Test recipe name"
-end
+    sign_up
+    log_in
+    create_recipe
+    click_button "Favourite"
+    visit "/mycookbook"
+    expect(page).to have_content "Test recipe name"
+    page.find(".recipe-card-liked-button").click
+    expect(page).to_not have_content "Test recipe name"
+  end
 end
