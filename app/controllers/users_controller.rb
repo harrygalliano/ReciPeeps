537ea4fac0245c2272872293497e55ff7020acb5
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-05-24 13:59:37 +0100

    STYLING: Tidied up images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @recipes = Recipe.where(user_id: @user.id).paginate(page: params[:page], per_page: 12)
   end
 
   def find
